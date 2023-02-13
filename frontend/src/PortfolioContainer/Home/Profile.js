91836d2803185c5fbb2ef765b0faafb7285609a4
mary3210 <maryrodri3210@gmail.com>
2023-02-13 16:28:31 -0600

    add links to corresponding icons

--- frontend/src/PortfolioContainer/Home/Profile.js
+++ frontend/src/PortfolioContainer/Home/Profile.js
@@ -8,11 +8,11 @@ function Profile() {
                     <a href='"'>
                         <i className= 'fa fa-facebook-square'></i>
                     </a> 
-                    <a href='"'>
-                        <i className= 'fa fa-facebook-square'></i>
+                    <a href='https://github.com/mary3210'>
+                    <i className="fa fa-github"></i>
                     </a> 
-                    <a href='"'>
-                        <i className= 'fa fa-facebook-square'></i>
+                    <a href='https://www.linkedin.com/in/mary-rodriguez-/'>
+                        <i className= 'fa fa-linkedin'></i>
                     </a> 
                     <a href='"'>
                         <i className= 'fa fa-facebook-square'></i>
