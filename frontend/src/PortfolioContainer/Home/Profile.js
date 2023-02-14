ee555a8cdf8bf96dccd5f09b95b6f9ba91152872
mary3210 <maryrodri3210@gmail.com>
2023-02-13 21:29:08 -0600

    added resume  button

--- frontend/src/PortfolioContainer/Home/Profile.js
+++ frontend/src/PortfolioContainer/Home/Profile.js
@@ -39,6 +39,15 @@ function Profile() {
             </span>
             </script>
           </div>
+          <div className="profile-options">
+            <button className="btn-primary-btn">
+                {""}
+                Hire Me {" "}
+                </button>
+            <a href='resume.pdf' download='Mary Rodriguez.pdf'>
+                <button className='btn-highlighted-btn'>Get Resume</button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
