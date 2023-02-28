b98d9e32e31055f4633ed23cb2fb205034735ed7
mary3210 <maryrodri3210@gmail.com>
2023-02-27 18:59:59 -0600

    imported about component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,13 @@
 import Profile from "./Profile";
 import Projects from "./Projects";
 import Footer from "../components/Footer";
+import About from "./About";
 function Home(props) {
     return (
       <div>
       <Profile/>
       <Projects/>
-     
+      
     </div>
     )
   }
